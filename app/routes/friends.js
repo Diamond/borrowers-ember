3f1cf45c81925e4d7bec4feb5e0106f9e2aff7be
Brandon Richey <diamondgfx@gmail.com>
2016-04-05 09:40:23 -0400

    WIP

---
+++ app/routes/friends.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    delete(friend) {
+      if (confirm("Are you sure you want to delete this record?")) {
+        friend.destroyRecord().then(() => {
+          this.transitionTo('friends.index');
+        })
+      }
+      return false;
+    }
+  }
+
+});
